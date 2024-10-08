55b7fa189529d509f4375bd3475ca6f2b30a7343
thientranreal <thientt2612@gmail.com>
2024-10-08 14:12:40 +0700

    fix cors

--- NotificationWebApi/Program.cs
+++ NotificationWebApi/Program.cs
@@ -6,18 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
-// Add CORS
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowSpecificOrigin",
-        builder =>
-        {
-            builder.WithOrigins("http://localhost:3000")
-                .AllowAnyMethod()
-                .AllowAnyHeader();
-        });
-});
-
 // Database Context Dependency Injection
 var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
 var dbName = Environment.GetEnvironmentVariable("DB_NAME");
@@ -26,21 +14,12 @@ var dbPassword = Environment.GetEnvironmentVariable("DB_ROOT_PASSWORD");
 var connectionString = $"server={dbHost};port=3306;database={dbName};user=root;password={dbPassword}";
 builder.Services.AddDbContext<NotificationDbContext>(o => o.UseMySQL(connectionString));
 //=========================================
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll",
-        policy => policy.AllowAnyOrigin()
-                        .AllowAnyMethod()
-                        .AllowAnyHeader());
-});
-var app = builder.Build();
 
-// Use CORS
-app.UseCors("AllowSpecificOrigin");
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
-app.UseCors("AllowAll");
+
 // Configure the HTTP request pipeline.
 
 app.UseAuthorization();
