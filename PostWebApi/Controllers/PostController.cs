c088760dd90e8fd20ba50120dfdc4bdd722080f4
PhamVanDu <phamvandu1m78@gmail.com>
2024-11-04 10:10:31 +0700

    update PostController

--- PostWebApi/Controllers/PostController.cs
+++ PostWebApi/Controllers/PostController.cs
@@ -1,4 +1,4 @@
-﻿using System.Text.RegularExpressions;
+﻿﻿using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PostWebApi.Models;
@@ -184,6 +184,29 @@ namespace PostWebApi.Controllers
              _dbContext.Posts.Remove(post);
              await _dbContext.SaveChangesAsync();
              return NoContent();
+        }
+
+        
+
+        // GET: api/post/search?content=example
+        [HttpGet("search")]
+        public async Task<ActionResult<IEnumerable<Post>>> SearchPostsByContent(string content)
+        {
+            if (string.IsNullOrEmpty(content))
+            {
+                return BadRequest("Content parameter is required.");
+            }
+
+            var posts = await _dbContext.Posts
+                .Where(post => post.content.Contains(content))
+                .ToListAsync();
+
+            if (posts == null || posts.Count == 0)
+            {
+                return NotFound($"No posts found with content containing: {content}.");
+            }
+
+            return Ok(posts); 
         }
     }
 }
